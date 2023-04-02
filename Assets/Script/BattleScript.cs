02cd170d6d6a546af29960695546d98f3634ae5b
DIMASRZH\dimas <dimas.rizky@shintavr.com>
2023-04-02 16:02:22 +0700

    no message

---
+++ Assets/Script/BattleScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleScript : MonoBehaviour
+{
+    public Player_Stats Player_Stats;
+    public Enemy_Stats Enemy_Stats;
+
+    public void Player_Correct()
+    {
+        
+    }
+
+    public void Player_Wrong()
+    {
+
+    }
+
+    public void Enemy_Correct()
+    {
+
+    }
+
+    public void Enemy_Wrong()
+    {
+
+    }
+
+}
