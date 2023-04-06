c5497903a9a34f66ee69dea39df53340c6cb1773
DIMASRZH\dimas <dimas.rizky@shintavr.com>
2023-04-06 08:12:58 +0700

    Import Asset

---
+++ Assets/Script/AnswerScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerScript : MonoBehaviour
+{
+    public bool IsCorrect = false;
+    public Manager Manager;
+    public BattleScript battlescript;
+
+    public void Answer()
+    {
+        if (battlescript.Turn == "Player")
+        {
+            if (IsCorrect)
+            {
+                battlescript.Player_Correct();
+                Manager.Correct();
+            }
+            else
+            {
+                battlescript.Player_Wrong();
+                Manager.Correct();
+
+            }
+        }
+        else if (battlescript.Turn == "Enemy")
+        {
+            if (IsCorrect)
+            {
+                battlescript.Enemy_Correct();
+                Manager.Correct();
+            }
+            else
+            {
+                battlescript.Enemy_Wrong();
+                Manager.Correct();
+            }
+        }
+    }
+
+}
